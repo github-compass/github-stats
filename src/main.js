3043d73dd373f594dbfd9daafc60e68643c105a8
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-15 13:17:50 -0800

    better logging

--- src/main.js
+++ src/main.js
@@ -4,17 +4,16 @@ import processAllStargazers from './processAllStargazers';
 import getAllStarsFromUser from './get-all-stars-from-user'
 import _ from 'lodash';
 import bunyan from 'bunyan';
-
-let log = bunyan.createLogger({
-  name: 'main',
-  streams: [{
-    path: '/Users/devin/github-compass/github-stats/bunyan.log'
-  }]
-});
-
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
+let log = bunyan.createLogger({
+  name: 'main',
+  streams: [{
+    path: path.resolve(__dirname + '/../logs/bunyan.log')
+  }]
+});
+
 let github = new GithubApi({
   debug: true,
   version: "3.0.0",
@@ -35,7 +34,10 @@ Promise.promisifyAll(github.users);
 Promise.promisifyAll(github.activity);
 
 
-getAllStarsFromUser(github, "dpastoor", 2, 100, false).then(res => {
-  console.log(res)
-});
+// getAllStarsFromUser(github, "dpastoor", 2, 100, false).then(res => {
+//   console.log(res)
+// });
 
+github.activity.starRepoAsync({user: "dpatoor", repo: "lpgitbook"})
+  .then(res => log.info("starred"))
+  .catch(err => log.warn("ERROR", {err}))
